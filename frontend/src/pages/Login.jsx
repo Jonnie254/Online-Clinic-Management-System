264d73eb3cc5ad44e5aa831004e30f4aea83ceb6
jonnie254 <johnkamanu254@proton.me>
2024-10-09 00:51:04 +0300

    add the my profile page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,6 @@ import React, {useState} from 'react'
 const Login = () => {
 
     const [state, setState] = useState('Sign Up');
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
